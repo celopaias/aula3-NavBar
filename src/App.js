01910cbcd3716aa547b69b72ab2e18e452ee450b
marcelo <marcelo.batista@callflex.net.br>
2023-06-20 22:00:57 -0300

    Exercício aula 3

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './Components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
