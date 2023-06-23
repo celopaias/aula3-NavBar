753bde9ede8d68bf6dd148bbbb162de055048464
marcelo <marcelo.batista@callflex.net.br>
2023-06-22 21:26:23 -0300

    Exercício aula 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Components/Navbar';
+import { ItemListContainer } from './Components/ItemListContainer';
 
 function App() {
+  const textGreeting = () => {
+    return <p>Hello Wooooorld!</p>;
+  };
   return (
     <div>
       <header>
         <NavBar />
       </header>
+      <div class="container">
+        <ItemListContainer greeting={textGreeting()} />
+      </div>
     </div>
   );
 }
---
+++ src/Components/ItemListContainer.js
@@ -0,0 +1,7 @@
+export const ItemListContainer = ({ greeting }) => {
+  return (
+    <div className="text-3xl w-full text-slate-500 pt-3 text-center">
+      {greeting}
+    </div>
+  );
+};
